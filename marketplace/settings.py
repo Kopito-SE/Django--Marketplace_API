fd6877162a956f2430dc13ec3379d511f932e135
Seth <seth73602@gmail.com>
2026-05-06 14:55:10 +0300

    Update settings.py

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -173,6 +173,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 AUTH_USER_MODEL = "users.User"
 
 SIMPLE_JWT = {
